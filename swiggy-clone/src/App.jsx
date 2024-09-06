1c40f73386d1b639c7d30eb941b988732b75c20f
Sahilk9211 <skhan20024@gmail.com>
2024-09-06 12:44:10 +0530

    Initial commit

---
+++ swiggy-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import { AiFillClockCircle } from "react-icons/ai";
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-extrabold text-yellow-500 text-9xl '>Hello<AiFillClockCircle className='text-blue-500'/></h1>
+      <h1 className='bg-slate-500'>Byb bye</h1>
+    </>
+  )
+}
+
+export default App
